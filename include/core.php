9fda1bde838940d7b03d901deeeba5fea41d8962
Jalil Nouri <nouri.6412@gmail.com>
2022-06-20 11:11:26 +0430

    develop

--- include/core.php
+++ include/core.php
@@ -116,8 +116,75 @@ class Admin_Woo_Invoice_Core
     function report()
     {
 ?>
-        <button id="generate_chart_invoice">گزارش</button>
-        <div id="pwpc-chart-area"></div>
+        <style>
+            .invoice-wrap {
+                padding: 20px;
+            }
+
+            .invoice-wrap button {
+                /* padding: 2px 6px 4px 6px;
+    cursor: pointer; */
+            }
+
+            .invoice-wrap .postbox {
+                padding: 10px;
+            }
+
+            .invoice-wrap .postbox .filter-box {
+                display: flex;
+            }
+
+            .invoice-wrap .postbox .content-box {
+                margin-top: 24px;
+            }
+
+            .invoice-wrap .postbox .invoice-field {
+                margin-bottom: 15px;
+    width: 30%;
+    padding: 10px;
+            }
+
+            .invoice-wrap .postbox .invoice-field button {
+              margin-top: 18px;
+            }
+
+            .invoice-wrap .postbox .invoice-field label {
+                display: block;
+            }
+
+            .invoice-wrap .postbox .invoice-field input,
+            .invoice-wrap .postbox .invoice-field select {
+                width: 100%;
+                margin-top: 5px;
+                max-width: 100%;
+            }
+        </style>
+        <div class="invoice-wrap">
+            <h2>گزارش پیش فاکتور</h2>
+            <div class="postbox">
+                <div class="filter-box">
+                    <div class="invoice-field">
+                        <label for="report-type">نوع گزارش</label>
+                        <select id="report-type" name="report-type">
+                            <option value="1">تعداد پیش فاکتور</option>
+                            <option value="2">مبلغ پیش فاکتور</option>
+                        </select>
+                    </div>
+                    <div class="invoice-field">
+                        <label>تعداد روز</label>
+                        <input type="number" />
+                    </div>
+                    <div class="invoice-field">
+                    <button class="button button-primary" id="generate_chart_invoice">گزارش</button>
+                    </div>
+            
+                </div>
+
+                <div class="content-box"></div>
+                <div id="pwpc-chart-area"></div>
+            </div>
+        </div>
+
     <?php
     }
 
