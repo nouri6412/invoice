1c1fcd911126474fdd52e6a5e60a0d95c4c1227a
Jalil Nouri <nouri.6412@gmail.com>
2022-06-21 16:28:57 +0430

    fix

--- include/core.php
+++ include/core.php
@@ -841,3 +841,33 @@ function modify_query_to_filter_by_author($query)
         }
     }
 }
+
+
+add_filter( 'manage_invoice-form_posts_columns', 'smashing_filter_posts_columns' );
+function smashing_filter_posts_columns( $columns ) {
+
+    $cols=[];
+    foreach($columns as $key=>$col)
+    {
+        if($key=="date")
+        {
+            $cols['contact'] = 'خریدار';
+            $cols['note'] = 'یادداشت';
+        }
+        $cols[$key]=$col;
+    }
+
+  return $cols;
+}
+
+
+add_action('manage_invoice-form_posts_custom_column', function ($column_key, $post_id) {
+    if ($column_key == 'contact') {
+        $contact = get_field('contact',$post_id);
+       echo $contact->post_title;
+    }
+    if ($column_key == 'note') {
+        $note = get_post_meta($post_id, 'note', true);
+       echo $note;
+    }
+}, 10, 2);
