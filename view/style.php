0966e55e5838a87d1dc170d024cff6d3decc76d0
Jalil Nouri <nouri.6412@gmail.com>
2022-07-03 12:44:06 +0430

    fix

---
+++ view/style.php
@@ -0,0 +1,121 @@
+<style>
+                .acf-input .acf-input-wrap {
+                position: relative;
+                overflow: initial;
+            }
+
+            .acf-input .acf-input-wrap .list-price {
+                position: absolute;
+                top: 30px;
+                border: 1px solid #b3b0b0;
+                width: 100%;
+                padding: 5px;
+                background: #fff;
+                z-index: 1000;
+            }
+
+            .acf-input .acf-input-wrap .list-price .item-price {
+                margin-bottom: 4px;
+                cursor: pointer;
+                display: flex;
+                width: 50%;
+                float: right;
+            }
+
+            .acf-input .acf-input-wrap .list-price .item-price .item-price-img {}
+
+            .acf-input .acf-input-wrap .list-price .item-price .item-price-img img {
+                width: 60px;
+                height: 60px;
+                object-fit: cover;
+            }
+
+            .acf-input .acf-input-wrap .list-price .item-price .item-price-title {
+                padding: 8px;
+            }
+
+            .acf-input .acf-input-wrap .list-price .item-price .item-price-price {
+                padding: 8px;
+            }
+
+            .acf-input .acf-input-wrap .list-price .item-price:hover {
+                background-color: #eee;
+            }
+
+            .table-kala .acf-button {
+                width: 0;
+                padding: 0 !important;
+                border: 0 !important;
+            }
+
+            .invoice-loader-img {
+                position: absolute;
+                left: 2px;
+                width: 21px;
+                top: 4px;
+            }
+
+            .title-product-list input {
+                width: 240px !important;
+            }
+
+            .title-product-list .title-label {}
+
+            .invoice-modal {
+                position: fixed;
+                width: 100%;
+                height: 100%;
+                top: 0;
+                display: none;
+            }
+
+            .invoice-modal .modal-box {
+                width: 50%;
+                background-color: #fff;
+                border: 1px dolid #000;
+                box-shadow: 10px 10px 10px #b3b0b0;
+                margin: auto;
+                margin-top: 30px;
+                padding: 30px;
+            }
+
+            .invoice-modal .modal-box .modal-box-header {
+                padding: 10px 30px 10px 30px;
+            }
+
+            .invoice-modal .modal-box .modal-box-body {
+                padding: 10px 30px 10px 30px;
+            }
+
+            .invoice-modal .modal-box .modal-box-footer {
+                padding: 10px 30px 10px 30px;
+            }
+
+            .invoice-box-btn {}
+
+            .invoice-btn {
+                cursor: pointer;
+                text-decoration: none;
+                padding: 5px;
+            }
+
+            .invoice-form .invoice-form-field {
+                margin-top: 10px;
+            }
+
+            .invoice-form .invoice-form-field label {
+                display: block;
+                font-weight: bold;
+                margin: 0 0 3px;
+                padding: 0;
+            }
+
+            .invoice-form .invoice-form-field input {
+                width: 100%;
+                padding: 4px 8px;
+                margin: 0;
+                box-sizing: border-box;
+                font-size: 14px;
+                line-height: 1.4;
+            }
+</style>
