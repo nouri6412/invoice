dcebf0bacb11b0d0cb9bdb33e445b76deeefa5fb
Jalil Nouri <nouri.6412@gmail.com>
2022-07-03 17:22:53 +0430

    123

---
+++ view/bot-1.php
@@ -0,0 +1,35 @@
+<?php
+
+set_time_limit(60);
+
+$filename="bot1.txt";
+$myfile = fopen($filename, "r") or die("Unable to open file!");
+$ex1 = fgets($myfile);
+fclose($myfile);
+
+if ($ex1 == 0) {
+    $myfile = fopen($filename, "w") or die("Unable to open file!");
+    fwrite($myfile, '1');
+    fclose($myfile);
+    $str = file_get_contents('https://hyperhse.com/job-fetch-torob?type=step1');
+    $myfile = fopen($filename, "w") or die("Unable to open file!");
+    fwrite($myfile, '0');
+    fclose($myfile);
+}
+else
+{
+    $myfile = fopen($filename, "w") or die("Unable to open file!");
+    $count=$ex1+1;
+    fwrite($myfile, $count);
+    fclose($myfile); 
+}
+
+if ($ex1 > 10)
+{
+    $myfile = fopen($filename, "w") or die("Unable to open file!");
+    fwrite($myfile,'0');
+    fclose($myfile); 
+}
+
+
+echo 'finish';
